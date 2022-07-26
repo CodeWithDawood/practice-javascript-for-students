524a503b17e0e6d79fe9abaefd2fa436138d7661
Code With Dawood <83602093+CodeWithDawood@users.noreply.github.com>
2022-07-26 14:36:44 +0500

    Add files via upload

---
+++ practice 1/app.js
@@ -0,0 +1,60 @@
+// var ab= 10;
+
+// function sum(a,b,c){
+    
+//     f=4;
+//     d=a+b+c+ab+f;
+//     // a5 b5 c6 ab10 f4 
+//     console.log("value of sum", d)
+// }
+
+
+// sum(5,5,6); // returns 30
+
+// cntrl + / comment "initial arr:"
+// var arr;
+// arr1=[1,2,3,4,5,6,7,8,9];
+// // arr10=[10,20,30,40,50,60,70,80,90];
+// // arr100=[100,200,300,400,500,600,700,800,900];
+// // arr1000=[1000,2000,3000,4000,500,6000,7000,8000,9000];
+
+// console.log( "initial arr1:", arr1)
+// console.log( "initial arr10:", arr10)
+// console.log( "initial arr100:",arr100)
+// console.log( "initial arr1000:", arr1000)
+
+// arr1=[1,2,3,4,5,6,7,8,9];
+// console.log( "initial arr1:", arr1);
+// arr1.unshift(80);
+// // var a= arr1.pop();
+// console.log( "2nd arr1:", arr1)
+// // console.log("pop value:",a)
+// var c = arr1.shift(0);
+// console.log( "3rd arr1:", arr1)
+// console.log("shift value:",c)
+// var abc;
+// console.log("abc:", abc)
+
+
+
+// arr1=[0,1,2,3,4,5,6,7,8,9];
+// console.log( "initial arr1:", arr1);
+// var a= arr1.splice(2,3,"hello world");
+// // var a= arr1.splice(2,3,"hello world");
+// console.log( "2nd arr1:", arr1)
+// console.log( "value of a:", a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
